aae3a180d3ee7daf0720107c3cd5f6d54f799a7a
9inevolt <9inevolt@twitch.tv>
2015-05-28 12:30:08 -0700

    move WordGen to new wordgen module

---
+++ bdgg/wordgen.py
@@ -0,0 +1,57 @@
+#pseudorandom name generator
+grammar = { '{lexpat}' : [  '{c}{v}{lexpat}',
+                            '{v}{c}{lexpat}',
+                            '{word}',
+                            ''
+                            ],
+            '{c}' : 'bcdfghjklmnpqrstvwxyz',
+            '{v}' : 'aeiouy',
+            '{word}' : [    '{c}{v}{c}{lexpat}',
+                            '{v}{c}{v}{lexpat}',
+                            ]
+            }
+grammarindex = sorted(grammar.keys())
+
+weights = { '{lexpat}' : [0.25,0.25,0.15,1],
+            '{c}' : [0.0477, 0.0477,0.0477,0.0477,0.0476,
+                    0.0476,0.0476,0.0476,0.0476,0.0476,
+                    0.0476,0.0476,0.0476,0.0476,0.0476,
+                    0.0476,0.0476,0.0476,0.0476,0.0476,
+                    0.0476
+                    ],
+            '{v}' : [   0.1667,0.1667,0.1667,0.1666,0.1667,
+                        0.1666
+                    ],
+            '{word}' : [0.5,0.5]
+            }
+
+def getToken(self, token):
+    num = random.random()
+    acc = 0.0
+    i = 0
+
+    while num > acc:
+        acc += WordGen.weights[token][i]
+        i += 1
+
+    i -= 1
+    return WordGen.grammar[token][i]
+
+
+def generate(self, rseed):
+    random.seed(rseed)
+    word = ''
+
+    while len(word) < 4:
+        word += '{word}'
+        flag = 1
+        while flag:
+            flag = 0
+            for key in WordGen.grammarindex:
+                index = word.find(key)
+
+                while index != -1:
+                    word = word.replace(key, self.getToken(key), 1)
+                    index = word.find(key)
+                    flag = 1
+    return word[:10]
